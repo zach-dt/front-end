02e547db87c7308bfa74caf6b4506fe321e8acc9
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-10-22 16:48:00 +0200

    removing redis cache

--- server.js
+++ server.js
@@ -26,14 +26,10 @@ global.acmws['response-error-probability'] = 0;
 app.use(helpers.rewriteSlash);
 app.use(metrics);
 app.use(express.static("public"));
-if(process.env.SESSION_REDIS) {
-    console.log('Using the redis based session manager');
-    app.use(session(config.session_redis));
-}
-else {
-    console.log('Using local session manager');
-    app.use(session(config.session));
-}
+
+console.log('Using local session manager');
+app.use(session(config.session));
+
 
 app.use(bodyParser.json());
 app.use(cookieParser());
